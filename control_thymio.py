c26fff469cc9d9fdecdac22606a651ad6edf7406
Ahmad Agbaria <ahmad.agbaria@siemens.com>
2022-08-14 13:51:54 +0300

    Control thymio via voice command

---
+++ control_thymio.py
@@ -0,0 +1,84 @@
+import queue
+import sounddevice as sd
+from vosk import Model, KaldiRecognizer
+import sys
+import json
+
+from thymio import Thymio
+
+def init_thymio():
+    print('To create new Thymio interface')
+    _thymio = Thymio()
+    print('To start Thymio interface')
+    try:
+        r = _thymio.start()
+        if r>0:
+            print('Thymio interface has been started successfully')
+            return _thymio
+        else:
+            print('Thymio interface could not be started')
+            return None
+    except Exception as ex :
+        print(str(ex))
+        return None
+
+_thymio = None
+
+## Download Model(s) from https://alphacephei.com/vosk/models
+MODEL_FOLDER=r".\vosk\models\vosk-model-en-us-0.22-lgraph"
+
+'''This script processes audio input from the microphone and displays the transcribed text.'''
+    
+# list all audio devices known to your system
+# You may uncomment the following 2 lines for debugging
+# print("Display input/output devices")
+# print(sd.query_devices())
+
+
+# get the samplerate - this is needed by the Kaldi recognizer
+device_info = sd.query_devices(sd.default.device[0], 'input')
+samplerate = int(device_info['default_samplerate'])
+
+# display the default input device
+print("===> Initial Default Device Number:{} Description: {}".format(sd.default.device[0], device_info))
+
+# setup queue and callback function
+q = queue.Queue()
+
+def recordCallback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
+    
+# build the model and recognizer objects.
+print("===> Build the model and recognizer objects.  This will take a few minutes.")
+model = Model(MODEL_FOLDER)
+recognizer = KaldiRecognizer(model, samplerate)
+recognizer.SetWords(False)
+
+
+print("===> Begin recording. Press Ctrl+C to stop the recording ")
+try:
+    with sd.RawInputStream(dtype='int16', channels=1,callback=recordCallback):
+        while True:
+            data = q.get()        
+            if recognizer.AcceptWaveform(data):
+                recognizerResult = recognizer.Result()
+                # convert the recognizerResult string into a dictionary  
+                resultDict = json.loads(recognizerResult)
+                if not resultDict.get("text", "") == "":
+                    #Ahmad - Integrate Thymio to speech recognition
+                    print(recognizerResult)
+                    #Try re-initialize the thymio interface
+                    if _thymio is None:
+                        _thymio = init_thymio()
+                    else:
+                        _thymio.on_command(resultDict.get("text", ""))
+                else:
+                    print("no input sound")
+            #sd.sleep(500)
+
+except KeyboardInterrupt:
+    print('===> Finished Recording')
+except Exception as e:
+    print(str(e))
---
+++ thymio.py
@@ -0,0 +1,213 @@
+from tdmclient import ClientAsync, aw
+from tdmclient.atranspiler import ATranspiler
+
+import os
+
+
+class Thymio:
+    CMD_F_NAME = os.environ.get('CMD_FNAME') or '../cmd.txt'
+    MAX_SPEED = 90
+    MIN_SPEED = 40
+
+    def __init__(self):
+        self.__isfwd = 0
+        self.__speed = 50
+        self.__node = None
+        self.__client = ClientAsync()
+        self.__build_cmd_map()
+
+    def __build_cmd_map(self):
+        # Last two items in each entry of the map is related to hand gesture
+        self.__cmd_map = {
+            'fwd': ['start', 'move', 'come','come here', 'forward', 'one'],
+            'back': ['back', 'backward', 'go back', 'four'],
+            'right': ['right', 'two'],
+            'left': ['left', 'lift', 'three'],
+            'stop': ['stop', 'stopped', 'hold', 'stuff', 'five'],
+            'speed': ['speed', 'speed up', 'fast', 'faster', 'quick', 'fist'],
+            'slow': ['slow', 'slower', 'calm down', 'slow down', 'y'],
+        }
+
+    def start(self):
+        self.__node = Thymio.__lock_robot__(self.__client)
+        if self.__node is not None:
+            Thymio.__play_system_sound(self.__node, 0)
+            return 1
+        else:
+            return 0
+
+    def circle_coloring(self, colors):
+        if self.__node is None:
+            print('Error: Robot is not initialized or not connected')
+            return -1
+        v = {
+            'leds.circle': [
+                colors[0], colors[1], colors[2], colors[3], colors[4],
+                colors[5], colors[6], colors[7]
+            ],
+        }
+        Thymio.__set_vars__(self.__node, v)
+        return 1
+
+    def coloring(self, r, g, b, istop=True):
+        if self.__node is None:
+            print('Error: Robot is not initialized or not connected')
+            return -1
+        v = {}
+        if istop == True:
+            v = {
+                'leds.top': [r, g, b],
+            }
+        else:
+            v = {
+                'leds.bottom': [r, g, b],
+            }
+
+        Thymio.__set_vars__(self.__node, v)
+        return 1
+    
+    def __any_a_in_b__(self,a,b):
+        return any(x in b for x in a)
+
+    def on_command(self, cmd):
+        if self.__node is None:
+            print('Error: Robot is not initialized or not connected')
+            return -1
+        if cmd is None or cmd == '':
+            print('Error: Empty command')
+            return -1
+
+        self.coloring(0, 255, 0)
+        #the dictation sometimes add "the" to the command
+        cmd = cmd.split(' ')
+
+        #speed up
+        if self.__any_a_in_b__(cmd,self.__cmd_map['speed'])==True:
+            if self.is_moving:
+                #Thymio.__play_system_sound(self.__node, 4)
+                self.speed += 20
+                self.speed = min(Thymio.MAX_SPEED, self.speed)
+                #keep moving fwd/back
+                Thymio.__move_robot__(self.__node, self.__isfwd * self.__speed,
+                                      self.__isfwd * self.__speed)
+                self.circle_coloring([100, 100, 100, 100, 100, 100, 100, 100])
+        #slow down
+        elif self.__any_a_in_b__(cmd,self.__cmd_map['slow'])==True:
+            if self.is_moving:
+                self.speed -= 10
+                self.speed = max(Thymio.MIN_SPEED, self.speed)
+                #keep moving fwd/back
+                Thymio.__move_robot__(self.__node, self.__isfwd * self.__speed,
+                                      self.__isfwd * self.__speed)
+                self.circle_coloring([10, 10, 10, 10, 10, 10, 10, 10])
+
+        #move fwd
+        elif self.__any_a_in_b__(cmd,self.__cmd_map['fwd'])==True:
+            self.__isfwd = 1
+            Thymio.__move_robot__(self.__node, self.__isfwd * self.__speed,
+                                  self.__isfwd * self.__speed)
+            self.circle_coloring(
+                [self.speed, self.speed, 0, 0, 0, 0, self.speed, self.speed])
+        #move back
+        elif self.__any_a_in_b__(cmd,self.__cmd_map['back'])==True:
+            self.__isfwd = -1
+            Thymio.__move_robot__(self.__node, self.__isfwd * self.__speed,
+                                  self.__isfwd * self.__speed)
+            self.circle_coloring(
+                [0, 0, self.speed, self.speed, self.speed, self.speed, 0, 0])
+        #right
+        elif self.__any_a_in_b__(cmd,self.__cmd_map['right'])==True:
+            if self.is_moving:
+                Thymio.__move_robot__(self.__node, self.__isfwd * self.__speed,
+                                      self.__isfwd * self.__speed // 4)
+                self.circle_coloring([
+                    self.speed, self.speed, self.speed, self.speed, 0, 0, 0, 0
+                ])
+        #left
+        elif self.__any_a_in_b__(cmd,self.__cmd_map['left'])==True:
+            if self.is_moving:
+                Thymio.__move_robot__(self.__node,
+                                      self.__isfwd * self.__speed // 4,
+                                      self.__isfwd * self.__speed)
+                self.circle_coloring([
+                    0, 0, 0, 0, self.speed, self.speed, self.speed, self.speed
+                ])
+        #stop
+        elif self.__any_a_in_b__(cmd,self.__cmd_map['stop'])==True:
+            self.__isfwd = 0  #not fwd not back
+            Thymio.__stop_robot__(self.__node)
+            self.circle_coloring([0, 0, 0, 0, 0, 0, 0, 0])
+        else:
+            #nothing
+            print('Skip unknown command: ', cmd)
+            self.coloring(255, 0, 0)
+        return 1
+
+    @property
+    def speed(self):
+        return self.__speed
+
+    @speed.setter
+    def speed(self, speed):
+        self.__speed = speed
+
+    @property
+    def is_forward(self):
+        return self.__isfwd > 0
+
+    @property
+    def is_moving(self):
+        return self.__isfwd != 0
+
+    def __lock_robot__(client):
+        node = aw(client.wait_for_node())
+        aw(node.lock())
+        return node
+
+    def __set_vars__(node, vars):
+        aw(node.set_variables(vars))
+
+    def __move_robot__(node, left, right):
+        v = {
+            "motor.left.target": [left],
+            "motor.right.target": [right],
+        }
+        Thymio.__set_vars__(node, v)
+
+    def __stop_robot__(node):
+        Thymio.__move_robot__(node, 0, 0)
+
+    def __play_system_sound(node, i):
+        p='nf_sound_system(' + str(i) + ')'
+        pp=ATranspiler.simple_transpile(p)
+        aw(node.compile(pp))
+        aw(node.run())
+
+    def __run_python_f(node, fname,v):
+        p=fname+'(' + str(v) + ')'
+        pp=ATranspiler.simple_transpile(p)
+        aw(node.compile(pp))
+        aw(node.run())
+
+
+
+        # with ClientAsync() as client:
+
+        #     async def prog():
+        #         await node.set_variables(vars)
+
+        #     client.run_async_program(prog)
+
+
+
+
+'''
+#0 start, 1 shutdown...4 fire
+#nf_sound_system(100)
+
+#nf_leds_sound(2)
+
+nf_sound_freq(800,50)
+
+nf_leds_top(200,0,0)
+'''
